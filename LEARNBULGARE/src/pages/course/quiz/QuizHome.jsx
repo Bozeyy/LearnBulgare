21da30850ba817df17ba23d38bfb4861cfadd923
Bozeyy <spereaux2@gmail.com>
2025-02-20 17:57:50 +0200

    clear: remove useless file with new generic objec

--- LEARNBULGARE/src/pages/course/quiz/QuizHome.jsx
+++ LEARNBULGARE/src/pages/course/quiz/QuizHome.jsx
@@ -26,7 +26,7 @@ function QuizHome() {
                     <Link to="/verbesQuiz_0_10">
                         <div className='course'>
                             <div className="icon_course">
-                                <img src="./images/alphabet.png" alt="" />
+                                <img src="./images/verbes.png" alt="" />
                             </div>
                             <div className="title_course">
                                 <h1 id="title_course">10 verbes Quizz</h1>
